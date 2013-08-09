21ca00cfc85b5561da3d3dabb62c7a7c00ee9cb4
Toby Inkster <mail@tobyinkster.co.uk>
2013-08-09 10:13:30 +0100

    META.json provides field

--- lib/Dist/Inkt/Profile/TOBYINK.pm
+++ lib/Dist/Inkt/Profile/TOBYINK.pm
@@ -12,6 +12,7 @@ with qw(
 	Dist::Inkt::Role::ProcessDOAP
 	Dist::Inkt::Role::ProcessDOAPDeps
 	Dist::Inkt::Role::CopyFiles
+	Dist::Inkt::Role::MetaProvides
 	Dist::Inkt::Role::WriteMakefilePL
 	Dist::Inkt::Role::WriteMetaJSON
 	Dist::Inkt::Role::WriteMetaYML
---
+++ lib/Dist/Inkt/Role/MetaProvides.pm
@@ -0,0 +1,24 @@
+package Dist::Inkt::Role::MetaProvides;
+
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = '0.001';
+
+use Moose::Role;
+use Module::Metadata;
+use namespace::autoclean;
+
+after PopulateMetadata => sub
+{
+	my $self = shift;	
+	$self->log("Finding provided packages");
+	
+	# XXX - should filter result using manifest_skip
+	my $provides = 'Module::Metadata'->provides(
+		version  => '2',
+		dir      => $self->sourcefile('lib'),
+	);
+	
+	$self->metadata->{provides} = $provides;
+};
+
+1;
