4e73e37a229f8657133edcebd20780cb164ef778
Toby Inkster <mail@tobyinkster.co.uk>
2013-09-11 08:38:07 +0100

    bump version; changelog

--- lib/Dist/Inkt/Role/MetaProvides.pm
+++ lib/Dist/Inkt/Role/MetaProvides.pm
@@ -1,7 +1,7 @@
 package Dist::Inkt::Role::MetaProvides;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.009';
+our $VERSION   = '0.010';
 
 use Moose::Role;
 use Module::Metadata;
--- lib/Dist/Inkt/Role/ReadMetaDir.pm
+++ lib/Dist/Inkt/Role/ReadMetaDir.pm
@@ -1,7 +1,7 @@
 package Dist::Inkt::Role::ReadMetaDir;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.009';
+our $VERSION   = '0.010';
 
 use Moose::Role;
 use RDF::TrineX::Functions 'parse';
