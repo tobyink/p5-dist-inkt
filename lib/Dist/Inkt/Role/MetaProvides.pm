c18d3020913c33f69534729d63a534088bd99b35
Toby Inkster <mail@tobyinkster.co.uk>
2014-06-19 12:40:47 +0100

    filter provides using no_index

--- lib/Dist/Inkt/Role/MetaProvides.pm
+++ lib/Dist/Inkt/Role/MetaProvides.pm
@@ -12,11 +12,37 @@ after PopulateMetadata => sub
 	my $self = shift;
 	$self->log("Finding provided packages");
 	
-	# XXX - should filter result using manifest_skip
 	my $provides = 'Module::Metadata'->provides(
 		version  => '2',
 		dir      => $self->sourcefile('lib'),
 	);
+	
+	for my $pkg ( @{ $self->metadata->{no_index}{package} ) {
+		delete $provides->{$pkg};
+	}
+	
+	for my $ns ( @{ $self->metadata->{no_index}{namespace} ) {
+		for my $pkg (keys %$provides) {
+			delete $provides->{$pkg}
+				if $pkg =~ m{^\Q$ns\E::};
+		}
+	}
+	
+	for my $dir ( @{ $self->metadata->{no_index}{directory} ) {
+		for my $pkg (keys %$provides) {
+			delete $provides->{$pkg}
+				if $provides->{file} =~ m{^\Q$dir\E/};
+		}
+	}
+	
+	for my $file ( @{ $self->metadata->{no_index}{file} ) {
+		for my $pkg (keys %$provides) {
+			delete $provides->{$pkg}
+				if $provides->{file} eq $file;
+		}
+	}
+	
+	# XXX - should also filter result using manifest_skip
 	
 	$self->metadata->{provides} = $provides;
 };
